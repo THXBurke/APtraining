018e5e050084b6f5aeec752e2adafc3d0ea01ea4
Rachel Burke <rachelburke@Rachels-MacBook-Air.local>
2017-05-19 11:38:29 -0700

    day 2 demo

---
+++ JS/snacks.js
@@ -0,0 +1,9 @@
+var snacks = [];
+//add an item
+snacks.push('pistachios');
+//add more than one item
+snacks.push('avocados', 'tomatoes', 'chips');
+//add to the front of the list with using the toUpperCase method
+snacks.unshift('coffee'.toUpperCase());
+//calling the function from day2app.js
+printList(snacks);
